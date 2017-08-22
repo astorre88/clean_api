43606aa735b9441b173b925d6e632a1ed3a6ad61
Dmitry Vysotsky <bigbenperl@gmail.com>
2017-08-22 16:05:14 +0300

    Init

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+  "net/http"
+  "encoding/json"
+  "os"
+  "time"
+
+  "github.com/gorilla/mux"
+  "github.com/gorilla/handlers"
+  "github.com/dgrijalva/jwt-go"
+  "github.com/auth0/go-jwt-middleware"
+)
+
+type Product struct {
+  Id int
+  Name string
+  Slug string
+  Description string
+}
+
+var products = []Product{
+  Product{Id: 1, Name: "Hover Shooters", Slug: "hover-shooters",
+  Description : "Shoot your way to the top on 14 different hoverboards"},
+  Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer",
+  Description : "Explore the depths of the sea in this one of a kind"},
+  Product{Id: 3, Name: "Dinosaur Park", Slug : "dinosaur-park",
+  Description : "Go back 65 million years in the past and ride a T-Rex"},
+  Product{Id: 4, Name: "Cars VR", Slug : "cars-vr",
+  Description: "Get behind the wheel of the fastest cars in the world."},
+  Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood",
+  Description : "Pick up the bow and arrow and master the art of archery"},
+  Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr",
+  Description : "Explore the seven wonders of the world in VR"},
+}
+
+var mySigningKey = []byte("secret")
+
+var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  token := jwt.New(jwt.SigningMethodHS256)
+
+  claims := make(jwt.MapClaims)
+  claims["admin"] = true
+  claims["name"] = "Ado Kukic"
+  claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+  token.Claims = claims
+
+  tokenString, _ := token.SignedString(mySigningKey)
+
+  w.Write([]byte(tokenString))
+})
+
+var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+  ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+    return mySigningKey, nil
+  },
+  SigningMethod: jwt.SigningMethodHS256,
+})
+
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("API is up and running"))
+})
+
+var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  payload, _ := json.Marshal(products)
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write([]byte(payload))
+})
+
+var AddFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  var product Product
+  vars := mux.Vars(r)
+  slug := vars["slug"]
+
+  for _, p := range products {
+    if p.Slug == slug {
+      product = p
+    }
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  if product.Slug != "" {
+    payload, _ := json.Marshal(product)
+    w.Write([]byte(payload))
+  } else {
+    w.Write([]byte("Product Not Found"))
+  }
+})
+
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Not Implemented"))
+})
+
+func main() {
+  r := mux.NewRouter()
+
+  r.Handle("/", http.FileServer(http.Dir("./views/")))
+
+  r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
+                              http.FileServer(http.Dir("./static/"))))
+
+  r.Handle("/get-token", GetTokenHandler).Methods("GET")
+
+  r.Handle("/status", StatusHandler).Methods("GET")
+  r.Handle("/products", jwtMiddleware.Handler(ProductsHandler)).Methods("GET")
+  r.Handle("/products/{slug}/feedback", jwtMiddleware.Handler(AddFeedbackHandler)).Methods("POST")
+
+  http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r))
+}
